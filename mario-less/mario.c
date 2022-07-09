75f5ed3f73c4855919501fe8dd2994511f644dba
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-09 15:17:50 +0000

    Sat, Jul 9, 2022, 3:17 PM +00:00

---
+++ mario-less/mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //while loop ask user the height he wants
+    int height = 0;
+    do
+    {
+    height = get_int("Height of the pyramid? (it must be between 1 and 8 inclusive) ");
+    }while (height < 1 && height > 8);
+    //for loop to build the pyramid
+    for(int i=1; i <= height; i++)
+    {
+        for(int j=0; j < height-i; j++)
+        {
+            printf(" ");
+        }
+        for(int j=height; j <= 1; j-- )
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
