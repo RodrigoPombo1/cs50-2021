046cc092c1ab80e66cccd3ae641674737a7e1386
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-09 18:43:15 +0000

    Sat, Jul 9, 2022, 6:43 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -15,7 +15,7 @@ int main(void)
         credit_card_number = get_long("Number: ");
     }
     while (credit_card_number < 0);
-    
+    int digits = (credit_card_number - credit_card_number % 10)/10
     if
     {
         if (check_VISA(credit_card_number))
