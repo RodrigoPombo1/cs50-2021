58e5bb28e620ea6c91a25c820aff39369f5d843a
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-09 18:01:50 +0000

    Sat, Jul 9, 2022, 6:01 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,9 +9,48 @@ int main(void)
         credit_card_number = get_long("Number: ");
     }
     while (credit_card_number < 999999999999);
-    
+
     if (not_invalid):
         if (first_two_numbers == ):
     else:
         printf("INVALID\n")
+}
+
+int check_VISA(int card_number)
+{
+
+    if ()
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int check_MASTERCARD(int card_number)
+{
+
+    if ()
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int check_VISA(int card_number)
+{
+
+    if ()
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
