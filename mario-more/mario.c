ed2922e7ee66ea02be7bf257542e3e4ad9584f54
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-09 15:44:15 +0000

    Sat, Jul 9, 2022, 3:44 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    //while loop ask user the height he wants
+    int height;
+    do
+    {
+        height = get_int("Height of the pyramid? (it must be between 1 and 8 inclusive) ");
+    }
+    while (height < 1 || height > 8);
+    //for loop to build the pyramid
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 0; j < height - i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+                for (int j = 0; j < height - i; j++)
+        {
+            printf("#");
+        }
+        for (int j = 0; j < i; j++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
 }
