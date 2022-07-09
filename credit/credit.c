2a61d45931e2c912704b39edd0dddc76a1c88d6c
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-09 17:50:46 +0000

    Sat, Jul 9, 2022, 5:50 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    long credit_card_number;
+    do
+    {
+        credit_card_number = get_long("Number: ");
+    }
+    while (credit_card_number)
 }
