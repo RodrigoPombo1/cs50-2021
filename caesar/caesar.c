fa8967386ce7ddcfdc6720849be70521cb981117
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-15 20:58:14 +0000

    Fri, Jul 15, 2022, 8:58 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,32 +6,43 @@
 int main(int argc, string argv[])
 {
 
-    if (argc == 2 && isalpha(argv[1]))
+    if (argc == 2)
     {
-        string plaintext = getstring("plaintext: ");
-        int k = argv[1];
-        string ciphertext = "";
-        for (int i = 0, len = strlen(plaintext); i < len; i++)
+        for (int i = 0, len = strlen(argv[1]); i < len; i++)
         {
-            if (isalpha(plaintext[i]))
-            {
-                if (islower(plaintext[i]))
-                {
-                    ciphertext = ciphertext + (((plaintext[i] + k) - 65) % 26) + 65;
-                }
-                else if (isupper(plaintext[i]))
-                {
-                    ciphertext = ciphertext + (((plaintext[i] + k) - 97) % 26) + 97;
-                }
-            }
-            else
-            {
-                ciphertext = ciphertext + plaintext[i];
-            }
+            
         }
-        printf("ciphertext: %s\n", ciphertext)
+        if
+        {
+            string plaintext = getstring("plaintext: ");
+            int k = argv[1];
+            string ciphertext = "";
+            for (int i = 0, len = strlen(plaintext); i < len; i++)
+            {
+                if (isalpha(plaintext[i]))
+                {
+                    if (islower(plaintext[i]))
+                    {
+                        ciphertext = ciphertext + (((plaintext[i] + k) - 65) % 26) + 65;
+                    }
+                    else if (isupper(plaintext[i]))
+                    {
+                        ciphertext = ciphertext + (((plaintext[i] + k) - 97) % 26) + 97;
+                    }
+                }
+                else
+                {
+                    ciphertext = ciphertext + plaintext[i];
+                }
+            }
+            printf("ciphertext: %s\n", ciphertext);
+        }
+        else
+        {
+            printf("Usage: ./caesar key\n");
+        }
     else
     {
-        printf("Usage: ./caesar key\n")
+        printf("Usage: ./caesar key\n");
     }
 }
