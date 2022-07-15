a2624404dc75d7dfbdfd0311f72e3caa4cbecf4d
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-15 20:49:28 +0000

    Fri, Jul 15, 2022, 8:49 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
@@ -8,7 +9,7 @@ int main(int argc, string argv[])
     if (strlen(argv[]) == 2 && isalpha(argv[2]))
     {
         string plaintext = getstring("plaintext: ");
-        string
+        int k = argv[2];
         string ciphertext = "";
         for (int i = 0, len = strlen(plaintext); i < len; i++)
         {
@@ -16,16 +17,16 @@ int main(int argc, string argv[])
             {
                 if (islower(plaintext[i]))
                 {
-                    ciphertext = ciphertext + (plaintext[i] + k) % 65
+                    ciphertext = ciphertext + (((plaintext[i] + k) - 65) % 26) + 65
                 }
                 else if (isupper(plaintext[i]))
                 {
-                    ciphertext = ciphertext + (plaintext[i] + k) % 97
+                    ciphertext = ciphertext + (((plaintext[i] + k) - 97) % 26) + 97;
                 }
             }
             else
             {
-                ciphertext = ciphertext + plaintext[i]
+                ciphertext = ciphertext + plaintext[i];
             }
         }
         printf("ciphertext: %s\n", ciphertext)
