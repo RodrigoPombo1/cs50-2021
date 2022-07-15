c243ae916f1666ce5ad291f472da3b9d6ba2beb7
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-15 20:50:58 +0000

    Fri, Jul 15, 2022, 8:50 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,7 +6,7 @@
 int main(int argc, string argv[])
 {
 
-    if (arrlen(argv[]) == 2 && isalpha(argv[2]))
+    if (strlen(argv[]) == 2 && isalpha(argv[2]))
     {
         string plaintext = getstring("plaintext: ");
         int k = argv[2];
