2a20e26e07da703962db0ab3d0fdf1c6eb370e33
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-15 17:09:48 +0000

    Fri, Jul 15, 2022, 5:09 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,5 +3,38 @@
 
 int main(void)
 {
-
+    string text = get_string("Text: ")
+    int letter = 0;
+    int word = 0;
+    int sentence = 0;
+    //check what is inside the string
+    for (i = 0, len = strlen(text); i < len; )
+    {
+        if letter
+        {
+            letter += 1;
+        }
+        else if space
+        {
+            word += 1;
+        }
+        else if
+        {
+            sentence += 1;
+        }
+    }
+    //grade calculation
+    int grade =
+    if 1 <= grade && grade <16
+    {
+        printf("Grade %i", grade);
+    }
+    else if grade < 1
+    {
+        printf("Before Grade 1");
+    }
+    else
+    {
+        printf("Grade 16+");
+    }
 }
