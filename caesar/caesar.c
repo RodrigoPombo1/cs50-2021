ef7128ecea637e1e02d6615e5bf01ad476d4a5fa
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-18 17:19:36 +0000

    Mon, Jul 18, 2022, 5:19 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,6 +5,7 @@
 
 int main(int argc, string argv[])
 {
+    //checks if the user made a mistake
     if (argc == 2)
     {
         int error = 0;
@@ -17,6 +18,7 @@ int main(int argc, string argv[])
         }
         if (error == 0)
         {
+            //gets the key and converts it from a string to an integer
             string plaintext = get_string("plaintext: ");
             int k = 0;
             for (int i = 0, len = strlen(argv[1]); i < len; i++)
@@ -24,10 +26,12 @@ int main(int argc, string argv[])
                 k = k + (int) argv[1][i] - 48;
             }
             string ciphertext = "";
+            //cycles through the entire text
             for (int i = 0, len = strlen(plaintext); i < len; i++)
             {
                 if (isalpha(plaintext[i]))
                 {
+                    //converts them to the cypertext
                     if (islower(plaintext[i]))
                     {
                         printf("%c",  (char) ((( (int) plaintext[i] + k) - 65) % 26) + 65);        //////////
@@ -38,6 +42,7 @@ int main(int argc, string argv[])
                         ciphertext = ciphertext + (char) ((( (int) plaintext[i] + k) - 97) % 26) + 97;
                     }
                 }
+                //adds other characters from the original text that aren't letters to the cyphertext
                 else
                 {
                     ciphertext = ciphertext + plaintext[i];
