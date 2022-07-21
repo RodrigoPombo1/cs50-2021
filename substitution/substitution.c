85dd74a85a303b55690ff9b788be246b8668244a
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-21 19:24:46 +0000

    Thu, Jul 21, 2022, 7:24 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -44,7 +44,8 @@ int main(int argc, string argv[])
                     string key = argv[1];
                     ///////////////////////////////////STARTS CONVERTING TO CYPHERTEXT/////////////////////////////////////////////////
                     string plaintext = get_string("plaintext: ");
-                    char cyphertext[strlen(plaintext)] = "";
+                    int strlength = strlen(plaintext);
+                    char cyphertext[strlength] = "";
                     //cycles through the entire text
                     for (int i = 0, len = strlen(plaintext); i < len; i++)
                     {
