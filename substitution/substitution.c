5a30878ed6b6e9438fecf661f8bd46adb9d6e249
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-21 12:37:37 +0000

    Thu, Jul 21, 2022, 12:37 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,9 +5,11 @@
 
 int main(int argc, string argv[])
 {
-    //checks if the user made a mistake
+    //checks if the user made the mistake of not putting 2 arguments
     if (argc == 2)
     {
+        //checks if the key the user provided contains 26 characters
+        //checks if all the characters are letters
         int error = 0;
         for (int i = 0, len = strlen(argv[1]); i < len; i++) // fazer um array e ir comparando ao array para ver se nao se repete
         {
