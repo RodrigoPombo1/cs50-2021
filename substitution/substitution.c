c98650216bf3c20fc9a687e478f196dfe6c4c5ef
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-21 20:58:01 +0000

    Thu, Jul 21, 2022, 8:58 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -35,7 +35,7 @@ int main(int argc, string argv[])
                     }
                     for (int j = 0; j < 26; j++)
                     {
-
+                        
                     }
                     aux[i] = lowercasekey[i];
                 }
