7e82a2ad1df9c96d078fc1dedd1d8db23fe4351c
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-21 12:35:55 +0000

    Thu, Jul 21, 2022, 12:35 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -16,41 +16,41 @@ int main(int argc, string argv[])
                 error ++;
             }
         }
-                if (error == 0)
+            if (error == 0)
+            {
+                //gets the key and converts it from a string to an integer
+                string plaintext = get_string("plaintext: ");
+                int k = 0;
+                for (int i = 0, len = strlen(argv[1]); i < len; i++)
                 {
-                    //gets the key and converts it from a string to an integer
-                    string plaintext = get_string("plaintext: ");
-                    int k = 0;
-                    for (int i = 0, len = strlen(argv[1]); i < len; i++)
+                    if (k == 0)
                     {
-                        if (k == 0)
-                        {
-                            k = k + (int) argv[1][i] - 48;
-                        }
-                        else
-                        {
-                            k = k * 10 * i + (int) argv[1][i] - 48;
-                        }
+                        k = k + (int) argv[1][i] - 48;
+                    }
+                    else
+                    {
+                        k = k * 10 * i + (int) argv[1][i] - 48;
+                    }
 
-                    }
-                    //cycles through the entire text
-                    for (int i = 0, len = strlen(plaintext); i < len; i++)
+                }
+                //cycles through the entire text
+                for (int i = 0, len = strlen(plaintext); i < len; i++)
+                {
+                    if (isalpha(plaintext[i]))
                     {
-                        if (isalpha(plaintext[i]))
+                        //converts the letters
+                        if (islower(plaintext[i]))
                         {
-                            //converts the letters
-                            if (islower(plaintext[i]))
-                            {
-                                plaintext[i] = (char) key[(((int) plaintext[i]) - 97)] + 97; //
-                            }
-                            else if (isupper(plaintext[i]))
-                            {
-                                plaintext[i] = (char)((((int) plaintext[i] + k) - 65) % 26) + 65; //
-                            }
+                            plaintext[i] = (char) key[(((int) plaintext[i]) - 97)] + 97; //
+                        }
+                        else if (isupper(plaintext[i]))
+                        {
+                            plaintext[i] = (char)((((int) plaintext[i] + k) - 65) % 26) + 65; //
                         }
                     }
-                    printf("ciphertext: %s\n", plaintext);
                 }
+                printf("ciphertext: %s\n", plaintext);
+            }
             else
             {
                 printf("Key must only contain alphabetic characters.\n");
