281d6628ef17fde696e28b25398a671af0d219ce
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-29 20:36:14 +0000

    Fri, Jul 29, 2022, 8:36 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -168,6 +168,7 @@ bool print_winner(void)
         int half = candidate_count / 2;
         if (candidates[i].votes > half)
         {
+            printf("%s\n", candidates[i].name);
             return true;
         }
     }
@@ -177,7 +178,9 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
     return 0;
 }
 
