794104a5762418e0355042350c3a8a35d527cee6
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-07-30 07:33:13 +0000

    Sat, Jul 30, 2022, 7:33 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -165,12 +165,20 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
+    int valid_candidate_count = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        int half = candidate_count / 2;
+        if (candidates[i].eliminated == false)
+        {
+            valid_candidate_count++;
+        }
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        int half = valid_candidate_count / 2;
         if (candidates[i].votes > half)
         {
-            printf("%s", candidates[i].name);
+            printf("%s\n", candidates[i].name);
             return true;
         }
     }
