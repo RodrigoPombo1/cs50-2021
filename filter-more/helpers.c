2e15812318d2193369267433725372f27d5c5afd
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-08-30 17:13:53 +0000

    Tue, Aug 30, 2022, 5:13 PM +00:00

---
+++ filter-more/helpers.c
@@ -0,0 +1,31 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    //selects pixel and averages it's green blue and red values and changes the green blue and red values to the average
+    //loops through the whole image
+    for
+    
+        //calculates the average of blue, green and red values
+        //changes the values to the average
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
