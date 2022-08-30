b2c56eae4ddecb2629056cf3311ec1db060002af
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-08-30 17:16:32 +0000

    Tue, Aug 30, 2022, 5:16 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -5,10 +5,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     //selects pixel and averages it's green blue and red values and changes the green blue and red values to the average
     //loops through the whole image
-    for
-    
+    //each row
+    for (int i = 0; i < height; i++)
+    {
+        //each column
+        for (int j = 0; j < width; j++)
+        {
         //calculates the average of blue, green and red values
         //changes the values to the average
+        }
+    }
     return;
 }
 
