83dd53939455b8c2432b9d750d979d473ed593a2
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-09-05 15:18:42 +0000

    Mon, Sep 5, 2022, 3:18 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,9 +8,11 @@ int main(int argc, char *argv[])
     //Repeat until end of card
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
+        int buffer[];
         //Read 512 bytes into buffer
         fread(, f)
         //If start of new JPEG
+        if (buffer[0] == || buffer[1] == || buffer[2] == || buffer[3] == )
             //If first JPEG
 
             //Else
