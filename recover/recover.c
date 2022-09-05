dad098a83dcfd906bfe3a49981c19b93e507d45b
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-09-05 15:01:26 +0000

    Mon, Sep 5, 2022, 3:01 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    //Open memory card
+    //Repeat until end of card
+        //Read 512 bytes into buffer
+        //If start of new JPEG
+            //If first JPEG
+
+            //Else
+        //
 }
