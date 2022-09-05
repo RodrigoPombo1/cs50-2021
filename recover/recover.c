ac2162b9e5164cb4f3ec3220de482c0f154908e2
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-09-05 15:25:30 +0000

    Mon, Sep 5, 2022, 3:25 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
             //If first JPEG
             if (counter == 0)
             {
-
+                
                 counter++;
             }
             //Else (not first JPEG)
