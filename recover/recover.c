305cafd2f6d76e927d3a5a3290c89953247beccd
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-09-05 15:24:37 +0000

    Mon, Sep 5, 2022, 3:24 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,23 +3,38 @@
 
 int main(int argc, char *argv[])
 {
+    int counter = 0;
     //Open memory card
     FILE *f = fopen(argv[1], "r");
     //Repeat until end of card
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
-        int buffer[];
+        buffer[];
         //Read 512 bytes into buffer
         fread(, f)
         //If start of new JPEG
-        if (buffer[0] == || buffer[1] == || buffer[2] == || buffer[3] == )
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
             //If first JPEG
+            if (counter == 0)
+            {
 
-            //Else
+                counter++;
+            }
+            //Else (not first JPEG)
+            else
+            {
+                //give file name
+                counter++;
+            }
+        }
         //Else
+        else
+        {
             //If already found JPEG
 
             //Else
+        }
     }
     //Close any remaining files
 }
