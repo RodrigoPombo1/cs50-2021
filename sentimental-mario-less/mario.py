5e8f36861a59544f08378b08d5a67a2c79d43818
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-09-29 18:08:15 +0000

    Thu, Sep 29, 2022, 6:08 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,6 @@
-# TODO
+height = int(input("Height: "))
+for i in range(1, height):
+    for j in range(1, height):
+        print(" " * (height - i) - j,)
+        print("#" * (height - j) + j,)
+    print("\n")
