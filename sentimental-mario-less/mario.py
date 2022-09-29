797e7461891a1039cab9b3edc3b9d8289679b65f
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-09-29 18:26:01 +0000

    Thu, Sep 29, 2022, 6:26 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,3 +1,6 @@
-height = int(input("Height: "))
+while True:
+    height = int(input("Height: "))
+    if 1 < height && height < 9:
+        break
 for i in range(1, height + 1):
     print(" " * (height - i) + "#" * i)
