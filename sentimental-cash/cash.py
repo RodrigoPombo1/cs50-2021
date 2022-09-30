b2e76bdcca3c5d97b790158821cb49e3f2fa2ce6
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-09-30 15:59:37 +0000

    Fri, Sep 30, 2022, 3:59 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -6,3 +6,15 @@ while True:
         print("Must be a positive number")
     except:
         print("You must input a decimal number")
+
+# calculates quarters
+quarters = cash % 0.25
+cash = cash - 0.25 * quarters
+# calculates dimes
+dimes = cash % 0.1
+cash = cash - 0.1 * dimes
+# calculates nickels
+nickels = cash % 0.05
+cash = cash - 0.5 * dimes
+
+print(quarters + dimes + nickels + cash)
