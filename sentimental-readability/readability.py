c4705e7099db17b2d31c23d7f9b091153e4a516b
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-09-30 17:04:03 +0000

    Fri, Sep 30, 2022, 5:04 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -8,12 +8,13 @@ word = 0
 sentence = 0
 for i in range(length):
     char = text[i]
-    if char == ' ':
+    if isalpha(char):
+        letter += 1
+        print(char)
+    elif char == ' ':
         word += 1
     elif char == '!' or char == '?' or char == '.':
         sentence += 1
-    else:
-        letter += 1
 grade = round(0.0588 * float(letter) / float(word) * 100 - 0.296 * float(sentence) / float(word) * 100 - 15.8)
 
 if 1 < grade < 16:
