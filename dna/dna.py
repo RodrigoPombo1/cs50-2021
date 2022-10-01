af60610dcb1b390d6f010b8113ab19098676f060
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-10-01 14:11:08 +0000

    Sat, Oct 1, 2022, 2:11 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -14,12 +14,6 @@ def main():
     data = csv.DictReader(f)
     keys = data.fieldnames
 
-    # convert the number of times the DNA appears to an int
-    for person in data:
-        for key in keys:
-            if key != "name":
-                person[key] = int(person[key])
-
     # TODO: Read DNA sequence file into a variable
     filename = sys.argv[2]
     f = open(filename)
@@ -34,11 +28,6 @@ def main():
             longest_matching.append(longest_match(sequence1[0], key))
 
     # TODO: Check database for matching profiles
-    filename = sys.argv[1]
-    f = open(filename)
-    data = csv.DictReader(f)
-    # it's necessary to open it again because it has already gone through it once and that uses it up
-    # I could have also stored the values into a list in the first time and use the list now
     for person in data:
         counter = 0
         matches = 0
