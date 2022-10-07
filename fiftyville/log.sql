252e55a160b3a2363b4db69a556eaca371c0dab0
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-10-07 14:59:41 +0000

    Fri, Oct 7, 2022, 2:59 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
-SELECT * FROM bakery_security_logs;
+SELECT * FROM interviews;
