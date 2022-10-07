c2b1ab43840dd59b1ff721b468ac5e0c027d7319
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-10-07 15:55:37 +0000

    Fri, Oct 7, 2022, 3:55 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,7 +6,7 @@ SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND 
 SELECT name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 
 --first interview clue
-SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour < 11;
+SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute < 30;
 --second interview clue
 SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street";
 --third interview clue
@@ -14,14 +14,15 @@ SELECT caller, receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day
 SELECT passport_number FROM passengers WHERE flight_id IN (SELECT flights.id  FROM flights WHERE year = 2021 AND month = 7 AND day = 29 ORDER BY hour, minute LIMIT 1);
 
 --get the name of all the people who have done these actions
-SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour < 11);
+SELECT name FROM people WHERE license_plate IN (SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute < 30));
 SELECT name FROM people WHERE people.id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street"));
 SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;)
 SELECT name FROM people WHERE people.passport_number IN (SELECT passengers.passport_number FROM passengers WHERE flight_id IN (SELECT flights.id  FROM flights WHERE year = 2021 AND month = 7 AND day = 29 ORDER BY hour, minute LIMIT 1));
 
 --match them to see if there is someone who has done all of them (it should be our thief)
 SELECT name FROM people
-WHERE name IN (SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour < 11)
-AND name IN (SELECT name FROM people WHERE people.id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street"))))
+WHERE name IN (SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute < 30))
+AND name IN (SELECT name FROM people WHERE people.id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street")))
 AND name IN (SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60))
 AND name IN (SELECT name FROM people WHERE people.passport_number IN (SELECT passengers.passport_number FROM passengers WHERE flight_id IN (SELECT flights.id  FROM flights WHERE year = 2021 AND month = 7 AND day = 29 ORDER BY hour, minute LIMIT 1)));
+--the result is Bruce
