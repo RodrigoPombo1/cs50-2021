9f4575432b3f9cc1303ab5a4cbcae287eb9c850f
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-10-07 15:41:45 +0000

    Fri, Oct 7, 2022, 3:41 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,7 +11,7 @@ SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 A
 SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street";
 --third interview clue
 SELECT caller, receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
---there is still the flight clue that I haven't explored
+SELECT passport_number FROM passengers WHERE flight_id IN 
 
 --get the name of all the people who have done these actions
 SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28);
