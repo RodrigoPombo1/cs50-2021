c8711cc19288c02932502d3ad3fe329b906361ff
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-10-07 15:37:09 +0000

    Fri, Oct 7, 2022, 3:37 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -16,7 +16,7 @@ SELECT caller, receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day
 --get the name of all the people who have done these actions
 SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28);
 SELECT name FROM people WHERE people.id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street"));
-SELECT name FROM people WHERE 
+SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;)
 
 --match them to see if there is someone who has done all of them (it should be our thief)
-SELECT name FROM people WHERE name IN (SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28) AND name IN (SELECT name FROM people WHERE people.id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street")))) AND name IN ;
+SELECT name FROM people WHERE name IN (SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28) AND name IN (SELECT name FROM people WHERE people.id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street")))) AND name IN (SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60));
